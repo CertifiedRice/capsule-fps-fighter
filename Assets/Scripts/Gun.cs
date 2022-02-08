6033b9684859923c9a66e9ad6b4c1fb47d6f5b42
BossFreind1234 <vincentredtube@gmail.com>
2022-02-08 18:35:38 -0500

    First commit

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float damge = 10f;
+    public float fireRate = 25f;
+    public float range = 100f;
+
+    public Camera fpsCam;
+    public ParticleSystem muzzelFlash;
+    public GameObject impactEffect;
+
+    public int maxAmmo = 10;
+    private int currentAmmo;
+    public float reloadTime = 1f;
+    private bool isReloading = false;
+
+    private float nextTimeToFire = 0f;
+    
+    
+    void Start()
+    {
+        if (currentAmmo == -1)
+            currentAmmo = maxAmmo;
+    }
+
+    void Update()
+    {
+        if (isReloading)
+            return;
+
+        if (currentAmmo <= 0)
+        {
+            StartCoroutine(Reload());
+            return;
+        }
+        
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1f / fireRate;
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        muzzelFlash.Play();
+
+        currentAmmo--;
+        
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null)
+            {
+                target.TakeDamage(100f);
+            }
+
+            GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGO, 1.5f);
+        }
+    }
+
+    IEnumerator Reload()
+    {
+        isReloading = true;
+        Debug.Log("A player is reloading");
+
+        yield return new WaitForSeconds(reloadTime);
+        
+        currentAmmo = maxAmmo;
+        isReloading = false;
+    }
+}
---
+++ Assets/Scripts/crouch.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class crouch : MonoBehaviour {
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            transform.localScale = new Vector3(1.2f, 0.9f, 1.2f);
+        }
+        if (Input.GetKeyUp(KeyCode.C))
+        {
+            transform.localScale = new Vector3(1.2f, 1.8f, 1.2f);
+        }
+    }
+}
