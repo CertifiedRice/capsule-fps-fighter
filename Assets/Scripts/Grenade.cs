fc677693068b782ef1e916be2b386738fdcb7d0c
BossFreind1234 <vincentredtube@gmail.com>
2022-02-08 20:16:04 -0500

    Added grenade

---
+++ Assets/Scripts/Grenade.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grenade : MonoBehaviour
+{
+    public float delay = 3f;
+    public float radius = 10f;
+    public float force = 200;
+
+    public GameObject explosionEffect;
+    
+    float countdown;
+    bool hasExploded = false;
+
+    private void Start()
+    {
+        countdown = delay;
+    }
+
+    private void Update()
+    {
+        countdown -= Time.deltaTime;
+        if (countdown <= 0f && !hasExploded)
+        {
+            Expload();
+            hasExploded = true;
+        }
+
+        void Expload()
+        {
+            Instantiate(explosionEffect, transform.position, transform.rotation);
+
+          Collider[] collidersToDestroy =  Physics.OverlapSphere(transform.position, radius);
+
+          foreach (Collider nearbyObject in collidersToDestroy)
+          {
+              // Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
+              //
+              // if (rb != null)
+              // {
+              //     rb.AddExplosionForce(force, transform.position, radius);
+              // }
+        }
+          
+          Collider[] collidersToMove =  Physics.OverlapSphere(transform.position, radius);
+
+          foreach (Collider nearbyObject in collidersToMove)
+          {
+              Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
+
+              if (rb != null)
+              {
+                  rb.AddExplosionForce(force, transform.position, radius);
+              }
+          }
+          
+          Destroy(gameObject);
+        }
+    }
+}
